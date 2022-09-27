04f357f864377ebd1e143f3e4ce7feac875060b8
mehdi0001 <mehdi.yah01@gmail.com>
2022-09-27 09:40:52 -0400

    amelioration main

--- Main.py
+++ Main.py
@@ -4,72 +4,65 @@ from Effecteur import *
 from capteur import *
 from ASP import *
 
-c = 100                          # Longueur d'un côté d'une case
-n = 5                           # Nombre de cases par ligne et par colonne
-cases = []                      # Liste contenant les objets cases
-
-fen = Tk()
-fen.title('nettoyage de map')
-
-
-dessin = Canvas(fen, width = 600, height = 600, bg = 'white')
-dessin.grid(row = 20, column = 20, columnspan=2, padx=10, pady=10)
-
-for ligne in range(n):          # Les cases de chaque ligne seront stockées dans "transit"
-    transit=[]
-    for colonne in range(n):    # Conception des cases d'une ligne
-        transit.append(dessin.create_rectangle(colonne*c+2, ligne*c+2, (colonne+1)*c+2, (ligne+1)*c+2))
-    cases.append(transit)       # Ajout de la ligne à la liste principale
-
-
-photo_pouss = PhotoImage(file='Image/poussiere.png')
-photo_bijoux = PhotoImage(file='Image/bijoux.png')
-photo_aspi = PhotoImage(file='Image/aspirateur.png')
 
 
 
 
-env = environement(dessin)
-f = File()
-D = ASP.aspirateur(Effecteur,env,f)
-e = Effecteur(env,D,dessin)
-e.generate_aspirateur(photo_aspi)
+def pictures():
+    c = 100                          # Longueur d'un côté d'une case
+    n = 5                           # Nombre de cases par ligne et par colonne
+    cases = []                      # Liste contenant les objets cases
+    fen = Tk()
+    fen.title('nettoyage de map')
+
+
+    dessin = Canvas(fen, width = 600, height = 600, bg = 'white')
+    dessin.grid(row = 20, column = 20, columnspan=2, padx=10, pady=10)
+
+    for ligne in range(n):          # Les cases de chaque ligne seront stockées dans "transit"
+        transit=[]
+        for colonne in range(n):    # Conception des cases d'une ligne
+            transit.append(dessin.create_rectangle(colonne*c+2, ligne*c+2, (colonne+1)*c+2, (ligne+1)*c+2))
+        cases.append(transit)       # Ajout de la ligne à la liste principale
+
+
+    photo_pouss = PhotoImage(file='Image/poussiere.png')
+    photo_bijoux = PhotoImage(file='Image/bijoux.png')
+    photo_aspi = PhotoImage(file='Image/aspirateur.png')
+    pictures = fen,dessin,photo_pouss,photo_bijoux,photo_aspi
+    return pictures
+
+def main():
+    fen,dessin,photo_pouss,photo_bijoux,photo_aspi = pictures()
+
+    env = environement(dessin)
+    f = File()
+    D = ASP.aspirateur(Effecteur,env,f)
+    e = Effecteur(env,D,dessin)
+    e.generate_aspirateur(photo_aspi)
 
 
 
-env.generate_Dirty(photo_pouss)
-a = env.generate_Bijoux(photo_bijoux)
-# dessin.delete(D)
-
-###### Non informe #####
-# D.nonInforme()
-# print(env.map)
-# env.affichage()
-# c = capteur()
-# R = c.capteurMap(env)
-# print(D.nearestBDI(R))
-
-##### Informe #####
-# position_finale = D.position_finale()
-# heuristique = D.heuristique(position_finale)
-# D.informe(heuristique , position_finale)
-# D.chemin(position_finale)
-# env.affichage()
+    env.generate_Dirty(photo_pouss)
+    env.generate_Dirty(photo_pouss)
+    a = env.generate_Bijoux(photo_bijoux)
+    # dessin.delete(D)
 
 
 
 
 
 
-c = capteur()
-R = c.capteurMap(env)
-#D.executionNonInf(R,e)
-D.executionInf(e)
+
+    capt = capteur()
+    R = capt.capteurMap(env)
+    D.executionNonInf(R,e)
+    #D.executionInf(e)
+    #dessin.delete(ALL)
+    #fen.mainloop()                  # Boucle d'attente des événements
 
 
-
-
-
+main()
 
 
 
@@ -82,8 +75,22 @@ dessin.create_image(20,20,anchor=NW,image=photo_aspi)
 """
 
 
+###### Non informe #####
+# D.nonInforme()
+# print(env.map)
+# env.affichage()
+# capt = capteur()
+# R = capt.capteurMap(env)
+# print(D.nearestBDI(R))
 
-#fen.mainloop()                  # Boucle d'attente des événements
+##### Informe #####
+# position_finale = D.position_finale()
+# heuristique = D.heuristique(position_finale)
+# D.informe(heuristique , position_finale)
+# D.chemin(position_finale)
+# env.affichage()
+
+
 
 
 
